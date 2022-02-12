4dd9e23eea9c78fc4fddc8a4bf087f1633b1d56c
mamay2343 <nikgomer972@gmail.com>
2022-02-12 12:00:02 +0300

    задание 2

--- 2ex.js
+++ 2ex.js
@@ -7,4 +7,20 @@ console.log(typeof b, b);
 let c = true;
 console.log(typeof c, c);
 
-let d = 
+let d 
+console.log(d);
+
+let e = Symbol('Abcd')
+console.log(typeof e, e);
+
+let x = Object(333);
+console.log(typeof x, x);
+
+let y = null;
+console.log(typeof y, y);
+
+function asd(){
+}
+console.log(typeof asd, asd);
+
+
