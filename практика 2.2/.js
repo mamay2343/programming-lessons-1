2be6f5e391b877b9217f8410f0239092f56f1bc5
mamay2343 <nikgomer972@gmail.com>
2022-04-04 10:49:08 +0300

    Сохраняет список дел с адреса https://jsonplaceholder.typicode.com/todos в  JSON файл

---
+++ практика 2.2/.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const axios = require('axios');
+axios.get("https://jsonplaceholder.typicode.com/todos").then(
+        response => {
+            const a = response.data;
+            console.log(response);
+            let json = JSON.stringify(a);
+            console.log(typeof json); // мы получили строку!
+            console.log(json);
+            fs.writeFile('zhp.json', json, function (err) {
+                if (err) return console.log(err);
+                console.log('json file has created');
+            });
+        }
+    )
+    .catch(function (error) {
+        console.log(error);
+    })
