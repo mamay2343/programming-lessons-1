cd9f27905bf4715e1ac891846c3e1038e7b85946
mamay2343 <nikgomer972@gmail.com>
2022-10-01 09:37:49 +0300

    Создаёт сайт магазина с книгами

--- Практика 13.09/2ex.js
+++ Практика 13.09/2ex.js
@@ -13,13 +13,66 @@ app.use(express.urlencoded({
 app.get('/:id', (req, res) => {
     let id = req.params.id;
     console.log(id);
-    res.sendFile(path.join(ROOT, id + 'ex.html'));
+    res.sendFile(path.join(ROOT, '2ex.html'));
 });
-app.post("/login",function (request, response) {
-            if (!request.body) return response.sendStatus(400);
-            console.log(request.body);
-            
-            response.sendFile(path.join(ROOT, 'semantika.html'));
+app.post("/login", function (request, response) {
+    if (!request.body) return response.sendStatus(400);
+    console.log(request.body);
+
+    response.sendFile(path.join(ROOT, 'semantika.html'));
 });
-   
-app.listen(3000, ()=> console.log("Сервер запущен..."));
+app.post("/buy", function (request, response) {
+    if (!request.body) return response.sendStatus(400);
+    console.log(request.body);
+
+    response.sendFile(path.join(ROOT, 'buy.html'));
+});
+app.post("/buyy", function (request, response) {
+    if (!request.body) return response.sendStatus(400);
+    console.log(request.body);
+
+    response.sendFile(path.join(ROOT, 'buyy.html'));
+});
+app.post("/buyyy", function (request, response) {
+    if (!request.body) return response.sendStatus(400);
+    console.log(request.body);
+
+    response.sendFile(path.join(ROOT, 'buyyy.html'));
+});
+app.post("/nuy", function (request, response) {
+    if (!request.body) return response.sendStatus(400);
+    console.log(request.body);
+
+    response.sendFile(path.join(ROOT, 'nuy.html'));
+});
+app.post("/muy", function (request, response) {
+    if (!request.body) return response.sendStatus(400);
+    console.log(request.body);
+
+    response.sendFile(path.join(ROOT, 'muy.html'));
+});
+app.post("/vuy", function (request, response) {
+    if (!request.body) return response.sendStatus(400);
+    console.log(request.body);
+
+    response.sendFile(path.join(ROOT, 'vuy.html'));
+});
+app.post("/cuy", function (request, response) {
+    if (!request.body) return response.sendStatus(400);
+    console.log(request.body);
+
+    response.sendFile(path.join(ROOT, 'cuy.html'));
+});
+app.post("/zuy", function (request, response) {
+    if (!request.body) return response.sendStatus(400);
+    console.log(request.body);
+
+    response.sendFile(path.join(ROOT, 'zuy.html'));
+});
+app.post("/.html", function (request, response) {
+    if (!request.body) return response.sendStatus(400);
+    console.log(request.body);
+
+    response.sendFile(path.join(ROOT, '.html'));
+});
+app.listen(3000, () => console.log("Сервер запущен..."));
